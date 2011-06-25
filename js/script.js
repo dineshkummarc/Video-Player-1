96d3a7ecd830cb4f58d41b55fae24dc53d3f94df
Polikis <contacto.mail99@gmail.com>
2011-06-25 22:02:30 +0200

    Added Controls
    index.html -> Added Controls
    style.css -> Styled controls
    script.js -> Added Support for Controls

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-document.addEventListener("load",updatePlayButton,false);
-
 /*Define Elements*/
 var video = document.getElementById("video");
-var playButton = document.getElementById('play/pause'); 
+var playButton = document.getElementById('play');
+var slider = document.getElementById('range');
+var volume = document.getElementById('volume') 
 /*Update Play/Pause button*/
 video.addEventListener("play", updatePlayButton,false);
 video.addEventListener("pause", updatePlayButton,false);
 video.addEventListener("ended", updatePlayButton,false);
+video.addEventListener("timeupdate",updateTime,false);
+slider.addEventListener("change",setTime,false);
+volume.addEventListener("change",setVolume,false);
+
+function load() {
+	slider.max = video.duration;
+	volume.value = video.volume*100;
+	/*Adjust Width*/
+	document.getElementById('controls').style.width = video.clientWidth;
+	document.getElementById('controls').style.top = "-" + document.getElementById('controls').clientHeight;
+}
 
 function updatePlayButton(){
 	if (video.paused || video.ended) {
@@ -24,4 +35,16 @@ function PlayPause() {
   	else {
     	video.pause();
   	}
+}
+
+function setTime(){
+	video.currentTime = slider.value;
+}
+
+function setVolume(){
+	video.volume = volume.value/100
+}
+
+function updateTime(){
+	slider.value =  video.currentTime;
 }
