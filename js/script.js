0bf0745a5bb7e62630fb9eb0dbc25a3621c957eb
Polikis <contacto.mail99@gmail.com>
2011-06-25 19:51:26 +0200

    Updated Play/Pause Button
    Now the play/pause button is dynamic, it changes accordingly when the video is playing or paused

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+document.addEventListener("load",updatePlayButton,false);
+
+/*Define Elements*/
+var video = document.getElementById("video");
+var playButton = document.getElementById('play/pause'); 
+/*Update Play/Pause button*/
+video.addEventListener("play", updatePlayButton,false);
+video.addEventListener("pause", updatePlayButton,false);
+video.addEventListener("ended", updatePlayButton,false);
+
+function updatePlayButton(){
+	if (video.paused || video.ended) {
+    	playButton.value = "Play";
+  	}
+  	else {
+    	playButton.value = "Pause";
+  	}
+}
+
 function PlayPause() {
-var video = document.getElementById("video"); 
-  if (video.paused || video.ended) {
-    video.play();
-  }
-  else {
-    video.pause();
-  }
+	if (video.paused || video.ended) {
+    	video.play();
+  	}
+  	else {
+    	video.pause();
+  	}
 }
